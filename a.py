7334a45807af8a676f0440f794886c737c54975f
riyanardyanto <riyanardyanto@gmail.com>
2025-05-18 21:19:47 +0700

    add spa_scraper_pyo3

--- a.py
+++ a.py
@@ -2,14 +2,14 @@ import json
 from pprint import pprint
 
 import httpx
-import spa_scraper
+import spa_scraper_pyo3
 
 
 def test_get_spa_data():
     # Mock the HTTPX client
-    mock_client = httpx.Client()
+    # mock_client = httpx.Client()
 
-    url = "http://127.0.0.1:5500/assets/page-result.html"
+    # url = "http://127.0.0.1:5500/assets/page-result.html"
 
     with open("assets/page-stop.html", "r") as file:
         html = file.read()
@@ -18,8 +18,11 @@ def test_get_spa_data():
     # html = response.text
 
     # Call the function to scrape stop statistics
-    result = spa_scraper.scrape_stop_stats(html)
-    pprint(result.items(), indent=2)
+    result = spa_scraper_pyo3.extract_stop_stats(html)
+    # pprint(result.to_python_dict())
+
+    with open("_stop_stats.json", "w") as f:
+        f.write(json.dumps(result.to_python_dict()))
 
 
 if __name__ == "__main__":
